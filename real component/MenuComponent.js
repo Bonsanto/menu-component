2552b1f6a731efebf166cca0410181ed4bfd0c6e
Favio Vazquez <favio.vazquezp@gmail.com>
2015-02-19 18:56:25 -0430

    -- fixed all errors and some extra animations added

--- real component/MenuComponent.js
+++ real component/MenuComponent.js
@@ -15,7 +15,7 @@ MenuProto.createdCallback = function () {
 	div.style.cursor = "default";
 	ul.className = "nav";
 	//todo: unfortunately this is how it is done :,(.
-	style.textContent = "* {padding: 0;margin: 0;} .realdiv {margin: auto;width: 500px; font-family: Arial, Helvetica, sans-serif;} ul, ol {list-style: none;} .nav li div {background-color: black;color: white;text-decoration: none;padding: 10px 15px;display: block;-webkit-touch-callout: none;-webkit-user-select: none;-khtml-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;} .nav > li {float: left;} .nav li div:hover {background-color: #434343;} .nav li ul {display: none;position: absolute;min-width: 150px;} .nav li:hover > ul {display: block;} .nav li ul li {position: relative;} .nav li ul li ul {right: -150px;top: 0;}";
+	style.textContent = "* {padding: 0;margin: 0;} .realdiv {margin: auto;width: 500px; font-family: Arial, Helvetica, sans-serif;} ul, ol {list-style: none;} .nav li div {background: linear-gradient(blue,black); background: -webkit-linear-gradient(black, blue); background: -o-linear-gradient(black, blue); background: -moz-linear-gradient(black, blue);color: white;text-decoration: none;padding: 10px 15px;display: block;-webkit-touch-callout: none;-webkit-user-select: none;-khtml-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;} .nav > li {float: left;} .nav li:hover > div{background:#434343;} .nav li div:hover {background: #434343;} .nav li ul {display: none;position: absolute;min-width: 150px;} .nav li:hover > ul {display: block;} .nav li ul li {position: relative;} .nav li ul li ul {right: -150px;top: 0;} .nav li ul li ul li ul li ul{right: -150px;top: 0;}";
 
 	shadow.appendChild(style);
 	div.appendChild(ul);

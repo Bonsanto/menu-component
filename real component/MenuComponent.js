eb4f9c4d1be5d1c9a762a5d42f861bfbd53db4c1
Bonsanto <xbonsx@gmail.com>
2015-02-18 22:31:42 -0430

    - It doesn't work perfectly due a favio's bug.

--- real component/MenuComponent.js
+++ real component/MenuComponent.js
@@ -13,9 +13,9 @@ MenuProto.createdCallback = function () {
 
 	div.className = "realdiv";
 	div.style.cursor = "default";
+	ul.className = "nav";
 	//todo: unfortunately this is how it is done :,(.
 	style.textContent = "* {padding: 0;margin: 0;} .realdiv {margin: auto;width: 500px; font-family: Arial, Helvetica, sans-serif;} ul, ol {list-style: none;} .nav li div {background-color: black;color: white;text-decoration: none;padding: 10px 15px;display: block;-webkit-touch-callout: none;-webkit-user-select: none;-khtml-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;} .nav > li {float: left;} .nav li div:hover {background-color: #434343;} .nav li ul {display: none;position: absolute;min-width: 40px;} .nav li:hover > ul {display: block;} .nav li ul li {position: relative;} .nav li ul li ul {right: -140px;top: 0;}";
-	ul.className = "nav";
 
 	shadow.appendChild(style);
 	div.appendChild(ul);
@@ -32,14 +32,15 @@ MenuProto.createdCallback = function () {
 var lidivCreator = function (xson) {
 	return xson.map(function (element) {
 		var li = document.createElement("li"),
-			div = document.createElement("div"),
-			ul = document.createElement("ul");
+			div = document.createElement("div");
 		div.textContent = element.name;
 		li.appendChild(div);
-		li.appendChild(ul);
 
 		console.log(element.children);
-		if(element.children !== null) {
+		if (element.children !== null) {
+			var ul = document.createElement("ul");
+			li.appendChild(ul);
+
 			lidivCreator(element.children).forEach(function (ele) {
 				ul.appendChild(ele);
 			});

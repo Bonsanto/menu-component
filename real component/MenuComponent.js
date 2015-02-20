82a1de640aa135020e2a9184dd066488f9f356d2
Bonsanto <xbonsx@gmail.com>
2015-02-19 19:30:33 -0430

    more things

--- real component/MenuComponent.js
+++ real component/MenuComponent.js
@@ -10,32 +10,38 @@ MenuProto.createdCallback = function () {
 	var style = document.createElement("style");
 	var div = document.createElement("div");
 	var ul = document.createElement("ul");
-	eval(readFiles(this)); //A settings variable appears.
 
-	div.className = "realdiv";
-	div.style.cursor = "default";
-	ul.className = "nav";
-	//todo: unfortunately this is how it is done :,(.
-	style.textContent = "* {padding: 0;margin: 0;} .realdiv {margin: auto;width: 500px; font-family: Arial, Helvetica, sans-serif;} ul, ol {list-style: none;} .nav li div {background-color: black;color: white;text-decoration: none;padding: 10px 15px;display: block;-webkit-touch-callout: none;-webkit-user-select: none;-khtml-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;} .nav > li {float: left;} .nav li div:hover {background-color: #434343;} .nav li ul {display: none;position: absolute;min-width: 150px;} .nav li:hover > ul {display: block;} .nav li ul li {position: relative;} .nav li ul li ul {right: -150px;top: 0;}";
+	var exe = function (something) {
+		eval(something);
+		div.className = "realdiv";
+		div.style.cursor = "default";
+		ul.className = "nav";
+		//todo: unfortunately this is how it is done :,(.
+		style.textContent = "* {padding: 0;margin: 0;} .realdiv {margin: auto;width: 500px; font-family: Arial, Helvetica, sans-serif;} ul, ol {list-style: none;} .nav li div {background-color: black;color: white;text-decoration: none;padding: 10px 15px;display: block;-webkit-touch-callout: none;-webkit-user-select: none;-khtml-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;} .nav > li {float: left;} .nav li div:hover {background-color: #434343;} .nav li ul {display: none;position: absolute;min-width: 150px;} .nav li:hover > ul {display: block;} .nav li ul li {position: relative;} .nav li ul li ul {right: -150px;top: 0;}";
 
-	shadow.appendChild(style);
-	div.appendChild(ul);
-	shadow.appendChild(div);
+		shadow.appendChild(style);
+		div.appendChild(ul);
+		shadow.appendChild(div);
 
-
-	lidivCreator(settings.header).forEach(function (element) {
-		ul.appendChild(element);
-	});
-};
-
-var readFiles = function (element) {
-	var xml = new XMLHttpRequest();
-
-	xml.onreadystatechange = function () {
-		if (xml.status === 200 && xml.readyState === 4) return xml.responseText;
+		lidivCreator(settings.header).forEach(function (element) {
+			ul.appendChild(element);
+		});
 	};
 
-	xml.open("GET", element.getAttribute("src"), false);
+	eval(readFiles(this, exe)); //A settings variable appears.
+};
+
+var readFiles = function (element, fun) {
+	var xml = new XMLHttpRequest();
+
+	xml.onreadystatechange = function (e) {
+		if (xml.status === 200 && xml.readyState === 4) fun(xml.responseText);
+	};
+
+	//xml.onreadystatechange = function () {
+	//};
+
+	xml.open("GET", element.getAttribute("src"), true);
 	xml.send();
 };
 
